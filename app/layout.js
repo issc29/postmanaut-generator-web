c585847397e8a15adad9fa15dd2e49bc794a240a
Isaac Cohen <issc29@gmail.com>
2023-12-11 23:12:18 -0500

    remove octocat

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Postman Octocat Generator',
-  description: 'Postman Octocat Generator',
+  title: 'Postman Postmanaut Generator',
+  description: 'Postman Postmanaut Generator',
 }
 
 export default function RootLayout({ children }) {
